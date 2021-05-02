599653cd15cb9ac0e653134be98f2fa62e9a3b99
Andrew Masih <armasih@outlook.com>
2021-05-02 21:19:10 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+/* getting object values, if you want to access values from an object, you can find it by its key */
+
+let Country =  {
+  name: "England",
+  population: 55.98,
+  language: "English",
+  primaryWeather: "cold and wet"
+
+
+};
+
+console.log (Country.name);
+console.log (Country.population);
+console.log (Country.language); /*using dot notation to access the value from the object */
